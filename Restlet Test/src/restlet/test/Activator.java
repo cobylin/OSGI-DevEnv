439c3ab141386cc66eebce1f6361f0a182ad438e
Coby Lin <cobylin@live.com>
2016-05-18 17:06:26 +0800

    add application class

--- Restlet Test/src/restlet/test/Activator.java
+++ Restlet Test/src/restlet/test/Activator.java
@@ -4,11 +4,13 @@ import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.restlet.Application;
 import org.restlet.Component;
+import org.restlet.Restlet;
 import org.restlet.Server;
 import org.restlet.data.Protocol;
 import org.restlet.resource.Get;
 import org.restlet.resource.ServerResource;
 import org.restlet.resource.Status;
+import org.restlet.routing.Router;
 
 public class Activator implements BundleActivator {
 
@@ -42,6 +44,14 @@ public class Activator implements BundleActivator {
 	
 	public static class TestApplication extends Application {
 		
+		@Override
+		public Restlet createInboundRoot() {
+			Router router = new Router(getContext());
+
+			router.attach("/app/test", TestResource.class);
+
+			return router;
+		}
 		
 	}
 	
