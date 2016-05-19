188971f8c16e820986335b36f6afb5f194285177
Coby Lin <cobylin@live.com>
2016-05-19 13:20:21 +0800

    new component

--- Restlet Test/src/restlet/test/Activator.java
+++ Restlet Test/src/restlet/test/Activator.java
@@ -7,9 +7,10 @@ import org.restlet.Component;
 import org.restlet.Restlet;
 import org.restlet.Server;
 import org.restlet.data.Protocol;
+import org.restlet.representation.Representation;
 import org.restlet.resource.Get;
+import org.restlet.resource.ResourceException;
 import org.restlet.resource.ServerResource;
-import org.restlet.resource.Status;
 import org.restlet.routing.Router;
 
 public class Activator implements BundleActivator {
@@ -28,9 +29,9 @@ public class Activator implements BundleActivator {
 		Activator.context = bundleContext;
 		System.out.println("Restlet Test Start");
 		
-		Component component = new Component();
+		Component component = new TestComponent();
 		
-		Server s = new Server(Protocol.HTTP, 8888, TestResource.class);
+		Server s = new Server(Protocol.HTTP, 8888, component);
 		s.start();
 	}
 
@@ -42,14 +43,19 @@ public class Activator implements BundleActivator {
 		Activator.context = null;
 	}
 	
+	public static class TestComponent extends Component {
+		public TestComponent() {
+			getDefaultHost().attach("/app", new TestApplication());
+		}
+	}
+	
+	
 	public static class TestApplication extends Application {
 		
 		@Override
 		public Restlet createInboundRoot() {
 			Router router = new Router(getContext());
-
-			router.attach("/app/test", TestResource.class);
-
+			router.attach("/test/{id}", TestResource.class);
 			return router;
 		}
 		
@@ -58,8 +64,20 @@ public class Activator implements BundleActivator {
 
 	public static class TestResource extends ServerResource {
 		
+		private Integer id;
+		
+		@Override
+		protected void doInit() throws ResourceException {
+			try {
+				id = Integer.valueOf((String)getRequestAttributes().get("id"));
+			} catch (Throwable t) {
+				throw new ResourceException(400, t);
+			}
+		}
+		
 		@Get("txt")
-		public String doGet() {
+		public String get(Representation rep) {
+			System.out.println(id);
 			return "hello world!";
 		}
 	}
